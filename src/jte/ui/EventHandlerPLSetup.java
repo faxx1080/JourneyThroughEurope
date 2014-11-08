a277b3f195ce258f7a6476b8c38d1f73fc81e459
Frank Migliorino <frank.migliorino@stonybrook.edu>
2014-11-08 16:31:21 -0500

    pre-method writing

---
+++ src/jte/ui/EventHandlerPLSetup.java
@@ -0,0 +1,29 @@
+/*
+ * (C) Frank Migliorino 2014
+ * CSE 219 Final Project
+ * Portions Copyright Paul Fodor
+ */
+package jte.ui;
+
+/**
+ *
+ * @author Frank Migliorino <frank.migliorino@stonybrook.edu>
+ */
+public class EventHandlerPLSetup {
+    private PlayerSetupDialog plset;
+    
+    public EventHandlerPLSetup(PlayerSetupDialog pls) {
+        plset = pls;
+    }
+
+    void numPlayersChanged() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void startGame() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
+}
