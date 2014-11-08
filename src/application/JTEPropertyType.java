a12b6e986d48ee69c7577379c3fb2bafd1c1f232
Frank <Frank@Frank-Laptop.cs.stonybrook.edu>
2014-11-07 20:41:26 -0500

    updated for more rb stuff

---
+++ src/application/JTEPropertyType.java
@@ -0,0 +1,19 @@
+/*
+ * (C) Frank Migliorino 2014
+ * CSE 219 Final Project
+ * Portions Copyright Paul Fodor
+ */
+package application;
+
+/**
+ *
+ * @author Frank
+ */
+public enum JTEPropertyType {
+    UI_PROPERTIES_FILE_NAME, PROPERTIES_SCHEMA_FILE_NAME,DATA_PATH,
+    RESOURCE_LOCATION,
+    
+    // Localized names
+    DEF_YES, DEF_NO, SP_START, SP_LOAD, SP_ABOUT, SP_KILL, DEF_ENSURE_EXIT, DEF_KILL_TITLE     ;
+    
+}
