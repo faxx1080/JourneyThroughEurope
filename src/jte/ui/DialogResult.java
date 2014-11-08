b872ec8d572fd6c1ee9708449b5001af12b73ec6
Frank <Frank@Frank-Laptop.cs.stonybrook.edu>
2014-11-07 23:10:38 -0500

    for use with dialog

---
+++ src/jte/ui/DialogResult.java
@@ -0,0 +1,14 @@
+/*
+ * (C) Frank Migliorino 2014
+ * CSE 219 Final Project
+ * Portions Copyright Paul Fodor
+ */
+package jte.ui;
+
+/**
+ *
+ * @author Frank
+ */
+public enum DialogResult {
+    RES_YES, RES_NO;
+}
