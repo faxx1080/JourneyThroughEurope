15284a76ed97c58c88c0fd3ad96b361a5f067b49
fmigliorino <fmigliorino@LT01.campus.stonybrook.edu>
2014-11-24 18:15:12 -0500

    Cheats

--- src/application/Main.java
+++ src/application/Main.java
@@ -68,7 +68,9 @@ public final class Main extends Application {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        if (args.length > 0 && args[0].equals("--debug")) {
+            DEBUG = true;
+        }
         launch(args);
     }
     
