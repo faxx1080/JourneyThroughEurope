cfb6145c7b34a5f0bc903cfb872c0193c8c3ca19
Frank Migliorino <frank.migliorino@stonybrook.edu>
2014-12-04 22:55:34 -0500

    fully loads all city data

--- src/jte/file/CityLoader.java
+++ src/jte/file/CityLoader.java
@@ -96,32 +96,60 @@ public class CityLoader {
             NamedNodeMap cityAttrib = city.getAttributes();
             // Pull attribs
             // if returned a null, then that node DNE.
-            String Sid, color, ScoordX, ScoordY, name, desc;
+            String Sid, color, ScoordX, ScoordY, name, desc, flightQuad;
+            int fpX, fpY, type, v1, v2, c1, c2;
+            
             Sid = cityAttrib.getNamedItem(props.getProperty(XML_CITYID)).getNodeValue();
             color = cityAttrib.getNamedItem(props.getProperty(XML_CITYCOLOR)).getNodeValue();
             ScoordX = cityAttrib.getNamedItem(props.getProperty(XML_CITYX)).getNodeValue();
             ScoordY = cityAttrib.getNamedItem(props.getProperty(XML_CITYY)).getNodeValue();
             name = cityAttrib.getNamedItem(props.getProperty(XML_CITYNAME)).getNodeValue();
+            flightQuad = cityAttrib.getNamedItem(props.getProperty(XML_CITYFLQUAD)).getNodeValue();
             Node descNode = cityAttrib.getNamedItem(props.getProperty(XML_CITYDESC));
+            Node nfpX, nfpY, ntype, nv1, nv2, nc1, nc2;
+            nfpX = cityAttrib.getNamedItem(props.getProperty(XML_CITYFX));
+            nfpY = cityAttrib.getNamedItem(props.getProperty(XML_CITYFY));
+            ntype = cityAttrib.getNamedItem(props.getProperty(XML_CITYTYPE));
+            nv1 = cityAttrib.getNamedItem(props.getProperty(XML_CITYTV1));
+            nv2 = cityAttrib.getNamedItem(props.getProperty(XML_CITYTV2));
+            nc1 = cityAttrib.getNamedItem(props.getProperty(XML_CITYTC1));
+            nc2 = cityAttrib.getNamedItem(props.getProperty(XML_CITYTC2));
             
-//            Color col;
-//            
-//            switch (color) {
-//                case "red":
-//                    col = Color.RED;
-//                    break;
-//                case "yellow":
-//                    col = Color.YELLOW;
-//                    break;
-//                case "green":
-//                    col = Color.GREEN;
-//                    break;
-//                default:
-//                    throw new IllegalArgumentException("Color not expected. Die.");
-//            }
-            
-            if (descNode == null) {desc = "";}
+            if (descNode == null) {desc = "";}      
             else {desc = descNode.getNodeValue();}
+            
+            //Flight plan
+            fpX = 0; fpY = 0;
+            boolean isAirport; int flightQuadNum;
+            try {
+                flightQuadNum = Integer.parseInt(flightQuad);
+                fpX = Integer.parseInt(nfpX.getNodeValue());
+                fpY = Integer.parseInt(nfpY.getNodeValue());
+            } catch (Exception e) {
+                flightQuadNum = 0;
+            }
+            isAirport = (flightQuadNum > 0 && flightQuadNum < 7);
+            
+            //instructions
+            type = -1;
+            v1 = -1; v2 = -1; c1 = -1; c2 = -1;
+            if (ntype != null) {
+                try {
+                    type = Integer.parseInt(ntype.getNodeValue());
+                    if (nv1 != null) {
+                        v1 = Integer.parseInt(nv1.getNodeValue());
+                    }
+                    if (nv2 != null) {
+                        v2 = Integer.parseInt(nv2.getNodeValue());
+                    }
+                    if (nc1 != null) {
+                        c1 = Integer.parseInt(nc1.getNodeValue());
+                    }
+                    if (nc2 != null) {
+                        c2 = Integer.parseInt(nc2.getNodeValue());
+                    }
+                } catch (DOMException | NumberFormatException e) {}
+            }
             
             int id, coordX, coordY;
             id = Integer.parseInt(Sid);
@@ -129,9 +157,13 @@ public class CityLoader {
             coordY = Integer.parseInt(ScoordY);
             
             Point2D coord = new Point2D(coordX, coordY);
+            Point2D fcord = new Point2D(-100, -100); //not possible to click
+            if (isAirport) {
+                fcord = new Point2D(fpX, fpY);
+            }
             
-            //TODO: Fix values for flightMap, isHarbor, isAirport
-            City nCity = new City(0, Point2D.ZERO, coord, id, desc, name, true, color);
+            City nCity = new City(0, Point2D.ZERO, coord, id, desc, name, isAirport, color, flightQuadNum, fcord, type, v1, v2, c1, c2);
+            
             //citiesList.add(nCity);
             citiesList.put(id, nCity);
         }
