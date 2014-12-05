dfaf690b87b52798580d2eb51c30bc59fb1870d8
Frank Migliorino <frank.migliorino@stonybrook.edu>
2014-12-04 22:56:23 -0500

    Contains all needed enums now

--- src/jte/game/InstructionTypes.java
+++ src/jte/game/InstructionTypes.java
@@ -17,8 +17,42 @@ public enum InstructionTypes {
     FORCEFLY,
     MISSTURN_MOVEON,
     ADD_CARD,
-    MOVE_LIMITER;
+    MOVE_LIMITER,THROWTWICE, NOTHING;
     
+    public static InstructionTypes intToType(int x) {
+        switch (x) {
+            case 0:
+                return MOVEON;
+            case 1:
+                return GOTO;
+            case 2:
+                return NEXTROUND_SCORE_ADD;
+            case 3:
+                return NEXTROUND_SCORE_MULT;
+            case 4:
+                return NEXTROUND_SCORE_SET;
+            case 5:
+                return GOAGAIN;
+            case 6:
+                return FORCEFLY;
+            case 7:
+                return MISSTURN_MOVEON;
+            case 8:
+                return ADD_CARD;
+            case 9:
+                return MOVE_LIMITER;
+            case 10:
+                return THROWTWICE;
+            default:
+                return NOTHING;
+        }
+    }
+    
+    /**
+     * -1 indicates nothing.
+     * @param type
+     * @return 
+     */
     public static int typeToInt(InstructionTypes type) {
         switch (type) {
             case MOVEON:
@@ -41,8 +75,10 @@ public enum InstructionTypes {
                 return 8;
             case MOVE_LIMITER:
                 return 9;
+            case THROWTWICE:
+                return 10;
             default:
-                throw new IllegalArgumentException("InstructionTypes error: int not mapped to type.");
+                return -1;
         }
     }
     
